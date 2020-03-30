a02bc85732566dae59a32b7f1749c2c958ebe337
Iheb-Haboubi <iheb.haboubi56@gmail.com>
2020-03-30 13:10:45 +0100

    add __repr__ method as per #2

--- script.py
+++ script.py
@@ -5,4 +5,9 @@ class Person:
         self.job = job
 
     def greet(self):
-        print(f"hello, my name is {self.name} and I'm {self.age} years old. I'm a {self.job}")
+        print(
+            f"hello, my name is {self.name} and I'm {self.age} years old. I'm a {self.job}"
+        )
+
+    def __repr__(self):
+        return f"name : {self.name}\nage : {self.age}\njob : {self.job}"
