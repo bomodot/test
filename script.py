7d6e96a0fad31596a3419f69d13488bea06ed06d
Iheb-Haboubi <iheb.haboubi56@gmail.com>
2020-03-30 12:32:55 +0100

    add greet method

--- script.py
+++ script.py
@@ -3,3 +3,6 @@ class Person:
         self.name = name
         self.age = age
         self.job = job
+
+    def greet(self):
+        print(f"hello, my name is {self.name} and I'm {self.age} years old")
